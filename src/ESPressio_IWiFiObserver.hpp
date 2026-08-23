1074b2d0b9fa1439c41e857598e1097f19a2baaf
Simon J Stuart <lakraven@gmail.com>
2026-08-23 16:27:26 +0200

    feat(#9): add WiFi observer contract

---
+++ src/ESPressio_IWiFiObserver.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ESPressio_IObserver.hpp>
+#include "ESPressio_WiFiTypes.hpp"
+
+namespace ESPressio::WiFi {
+
+class IWiFiObserver : public virtual Observable::IObserver {
+public:
+    virtual ~IWiFiObserver() = default;
+    virtual void OnWiFiModeChanged(WiFiMode, WiFiMode) {}
+    virtual void OnClientStateChanged(const ClientRuntimeState&, const ClientRuntimeState&) {}
+    virtual void OnAccessPointStateChanged(const AccessPointRuntimeState&, const AccessPointRuntimeState&) {}
+    virtual void OnScanStateChanged(ScanState, ScanState) {}
+    virtual void OnScanCompleted(const std::vector<ScanResult>&) {}
+    virtual void OnAccessPointStationConnected(const MacAddress&) {}
+    virtual void OnAccessPointStationDisconnected(const MacAddress&) {}
+    virtual void OnClientIPAddressAcquired(const NetworkAddress&) {}
+    virtual void OnClientIPAddressLost() {}
+};
+
+} // namespace ESPressio::WiFi
