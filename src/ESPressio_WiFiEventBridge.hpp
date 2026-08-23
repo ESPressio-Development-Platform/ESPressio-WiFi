255426b550c7f0ca0915a9601d8ca52dfba3348a
Simon J Stuart <lakraven@gmail.com>
2026-08-23 17:05:47 +0200

    feat(#9): bridge all WiFi logical states to Events

--- src/ESPressio_WiFiEventBridge.hpp
+++ src/ESPressio_WiFiEventBridge.hpp
@@ -14,31 +14,55 @@ public:
         _initialized = static_cast<bool>(_observer);
         return _initialized;
     }
-    void Shutdown() { _observer.reset(); _initialized = false; }
+
+    void Shutdown() {
+        _observer.reset();
+        _initialized = false;
+    }
+
     bool IsInitialized() const noexcept { return _initialized; }
 
     void OnWiFiModeChanged(WiFi::WiFiMode before, WiFi::WiFiMode after) override {
         (new WiFiModeChangedEvent(before, after))->Queue();
     }
-    void OnClientStateChanged(const WiFi::ClientRuntimeState& before, const WiFi::ClientRuntimeState& after) override {
+
+    void OnClientStateChanged(
+        const WiFi::ClientRuntimeState& before,
+        const WiFi::ClientRuntimeState& after
+    ) override {
         (new WiFiClientStateChangedEvent(before, after))->Queue();
     }
-    void OnAccessPointStateChanged(const WiFi::AccessPointRuntimeState& before, const WiFi::AccessPointRuntimeState& after) override {
+
+    void OnAccessPointStateChanged(
+        const WiFi::AccessPointRuntimeState& before,
+        const WiFi::AccessPointRuntimeState& after
+    ) override {
         (new WiFiAccessPointStateChangedEvent(before, after))->Queue();
     }
+
+    void OnScanStateChanged(WiFi::ScanState before, WiFi::ScanState after) override {
+        (new WiFiScanStateChangedEvent(before, after))->Queue();
+    }
+
     void OnScanCompleted(const std::vector<WiFi::ScanResult>& results) override {
         (new WiFiScanCompletedEvent(results))->Queue();
     }
+
     void OnAccessPointStationConnected(const WiFi::MacAddress& station) override {
         (new WiFiAccessPointStationConnectedEvent(station))->Queue();
     }
+
     void OnAccessPointStationDisconnected(const WiFi::MacAddress& station) override {
         (new WiFiAccessPointStationDisconnectedEvent(station))->Queue();
     }
+
     void OnClientIPAddressAcquired(const WiFi::NetworkAddress& network) override {
         (new WiFiClientIPAddressAcquiredEvent(network))->Queue();
     }
-    void OnClientIPAddressLost() override { (new WiFiClientIPAddressLostEvent())->Queue(); }
+
+    void OnClientIPAddressLost() override {
+        (new WiFiClientIPAddressLostEvent())->Queue();
+    }
 
 private:
     Observable::ObserverHandlePtr _observer;
