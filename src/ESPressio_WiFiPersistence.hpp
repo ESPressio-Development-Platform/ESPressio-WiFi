846e4c2b55a99755b3864ca9cf840babd7c37e05
Simon J Stuart <lakraven@gmail.com>
2026-08-23 16:33:41 +0200

    feat(#9): support persistence of extended WiFi configuration types

--- src/ESPressio_WiFiPersistence.hpp
+++ src/ESPressio_WiFiPersistence.hpp
@@ -1,45 +1,40 @@
 #pragma once
 
 #include <ESPressio_Persistence_Serializable.hpp>
-#include "ESPressio_WiFiConfiguration.hpp"
 
 namespace ESPressio::WiFi {
 
 class WiFiConfigurationStore {
 public:
+    template<typename TConfiguration>
     static Persistence::SerializablePersistenceResult Save(
-        Persistence::IFileStorage& storage,
-        const char* path,
-        const WiFiConfiguration& configuration,
-        const Persistence::SerializablePersistenceOptions& options = {}
-    ) {
+        Persistence::IFileStorage& storage, const char* path,
+        const TConfiguration& configuration,
+        const Persistence::SerializablePersistenceOptions& options = {}) {
         return Persistence::SaveSerializable(storage, path, configuration, options);
     }
 
+    template<typename TConfiguration>
     static Persistence::SerializablePersistenceResult Load(
-        Persistence::IFileStorage& storage,
-        const char* path,
-        WiFiConfiguration& configuration,
-        const Persistence::SerializablePersistenceOptions& options = {}
-    ) {
+        Persistence::IFileStorage& storage, const char* path,
+        TConfiguration& configuration,
+        const Persistence::SerializablePersistenceOptions& options = {}) {
         return Persistence::LoadSerializable(storage, path, configuration, options);
     }
 
+    template<typename TConfiguration>
     static Persistence::SerializablePersistenceResult Save(
-        Persistence::IKeyValueStorage& storage,
-        const char* key,
-        const WiFiConfiguration& configuration,
-        const Persistence::SerializablePersistenceOptions& options = {}
-    ) {
+        Persistence::IKeyValueStorage& storage, const char* key,
+        const TConfiguration& configuration,
+        const Persistence::SerializablePersistenceOptions& options = {}) {
         return Persistence::SaveSerializable(storage, key, configuration, options);
     }
 
+    template<typename TConfiguration>
     static Persistence::SerializablePersistenceResult Load(
-        Persistence::IKeyValueStorage& storage,
-        const char* key,
-        WiFiConfiguration& configuration,
-        const Persistence::SerializablePersistenceOptions& options = {}
-    ) {
+        Persistence::IKeyValueStorage& storage, const char* key,
+        TConfiguration& configuration,
+        const Persistence::SerializablePersistenceOptions& options = {}) {
         return Persistence::LoadSerializable(storage, key, configuration, options);
     }
 };
