59b98dba292ad143758b9434671ea924c1f0a3a0
Simon J Stuart <lakraven@gmail.com>
2026-08-23 16:38:49 +0200

    fix(#9): define Serializable enum mappings at valid namespace scope

--- src/ESPressio_WiFiTypes.hpp
+++ src/ESPressio_WiFiTypes.hpp
@@ -4,7 +4,6 @@
 #include <cstdint>
 #include <string>
 #include <vector>
-
 #include <ESPressio_Serializable.hpp>
 
 namespace ESPressio::WiFi {
@@ -16,59 +15,32 @@ enum class ScanState : uint8_t { Idle, Scanning, Complete, Failed };
 enum class NetworkSecurity : uint8_t { Open, WEP, WPA, WPA2, WPA_WPA2, WPA3, WPA2_WPA3, Unknown };
 enum class AddressMode : uint8_t { DHCP, Static };
 
-ESPRESSIO_ENUM_MAPPING(
-    WiFiMode,
-    ESPRESSIO_ENUM_VALUE(WiFiMode::Disabled, "disabled"),
-    ESPRESSIO_ENUM_VALUE(WiFiMode::Client, "client"),
-    ESPRESSIO_ENUM_VALUE(WiFiMode::AccessPoint, "access-point"),
-    ESPRESSIO_ENUM_VALUE(WiFiMode::AccessPointClient, "access-point-client")
-)
-
-ESPRESSIO_ENUM_MAPPING(
-    AddressMode,
-    ESPRESSIO_ENUM_VALUE(AddressMode::DHCP, "dhcp"),
-    ESPRESSIO_ENUM_VALUE(AddressMode::Static, "static")
-)
-
 struct IPv4Address final : Serializable::Serializable<IPv4Address> {
     ESPRESSIO_SERIALIZABLE_TYPE(IPv4Address)
     ESPRESSIO_SERIALIZABLE_SCHEMA_VERSION(1)
-
-    std::array<uint8_t, 4> Octets{};
-
-    constexpr IPv4Address() = default;
-    constexpr IPv4Address(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
-        : Octets{{a, b, c, d}} {}
-
-    ESPRESSIO_SERIALIZABLE_PROPERTIES(
-        ESPRESSIO_PROPERTY("octets", Octets)
-    )
-
-    constexpr bool operator==(const IPv4Address& other) const noexcept { return Octets == other.Octets; }
-    constexpr bool operator!=(const IPv4Address& other) const noexcept { return !(*this == other); }
-
-    std::string ToString() const {
-        return std::to_string(Octets[0]) + "." + std::to_string(Octets[1]) + "." +
-            std::to_string(Octets[2]) + "." + std::to_string(Octets[3]);
-    }
+    std::array<uint8_t,4> Octets{};
+    IPv4Address() = default;
+    IPv4Address(uint8_t a,uint8_t b,uint8_t c,uint8_t d) : Octets{{a,b,c,d}} {}
+    ESPRESSIO_SERIALIZABLE_PROPERTIES(ESPRESSIO_PROPERTY("octets", Octets))
+    bool operator==(const IPv4Address& other) const noexcept { return Octets==other.Octets; }
+    bool operator!=(const IPv4Address& other) const noexcept { return !(*this==other); }
+    std::string ToString() const { return std::to_string(Octets[0])+"."+std::to_string(Octets[1])+"."+std::to_string(Octets[2])+"."+std::to_string(Octets[3]); }
 };
 
 struct MacAddress {
-    std::array<uint8_t, 6> Octets{};
-    constexpr bool operator==(const MacAddress& other) const noexcept { return Octets == other.Octets; }
-    constexpr bool operator!=(const MacAddress& other) const noexcept { return !(*this == other); }
+    std::array<uint8_t,6> Octets{};
+    bool operator==(const MacAddress& other) const noexcept { return Octets==other.Octets; }
+    bool operator!=(const MacAddress& other) const noexcept { return !(*this==other); }
 };
 
 struct NetworkAddress final : Serializable::Serializable<NetworkAddress> {
     ESPRESSIO_SERIALIZABLE_TYPE(NetworkAddress)
     ESPRESSIO_SERIALIZABLE_SCHEMA_VERSION(1)
-
     IPv4Address Address{};
     IPv4Address Gateway{};
-    IPv4Address SubnetMask{255, 255, 255, 0};
+    IPv4Address SubnetMask{255,255,255,0};
     IPv4Address PrimaryDNS{};
     IPv4Address SecondaryDNS{};
-
     ESPRESSIO_SERIALIZABLE_PROPERTIES(
         ESPRESSIO_PROPERTY("address", Address),
         ESPRESSIO_PROPERTY("gateway", Gateway),
@@ -78,39 +50,23 @@ struct NetworkAddress final : Serializable::Serializable<NetworkAddress> {
     )
 };
 
-struct ScanResult {
-    std::string SSID;
-    MacAddress BSSID{};
-    int32_t RSSI = 0;
-    uint8_t Channel = 0;
-    NetworkSecurity Security = NetworkSecurity::Unknown;
-    bool Hidden = false;
-};
-
-struct ClientRuntimeState {
-    ClientState State = ClientState::Disabled;
-    std::string SSID;
-    MacAddress BSSID{};
-    int32_t RSSI = 0;
-    uint8_t Channel = 0;
-    NetworkAddress Network{};
-    uint32_t ReconnectAttempt = 0;
-};
-
-struct AccessPointRuntimeState {
-    AccessPointState State = AccessPointState::Disabled;
-    std::string SSID;
-    uint8_t Channel = 0;
-    NetworkAddress Network{};
-    uint16_t ConnectedStations = 0;
-};
-
-struct WiFiRuntimeState {
-    WiFiMode Mode = WiFiMode::Disabled;
-    ClientRuntimeState Client{};
-    AccessPointRuntimeState AccessPoint{};
-    ScanState Scan = ScanState::Idle;
-    uint64_t Revision = 0;
-};
+struct ScanResult { std::string SSID; MacAddress BSSID{}; int32_t RSSI=0; uint8_t Channel=0; NetworkSecurity Security=NetworkSecurity::Unknown; bool Hidden=false; };
+struct ClientRuntimeState { ClientState State=ClientState::Disabled; std::string SSID; MacAddress BSSID{}; int32_t RSSI=0; uint8_t Channel=0; NetworkAddress Network{}; uint32_t ReconnectAttempt=0; };
+struct AccessPointRuntimeState { AccessPointState State=AccessPointState::Disabled; std::string SSID; uint8_t Channel=0; NetworkAddress Network{}; uint16_t ConnectedStations=0; };
+struct WiFiRuntimeState { WiFiMode Mode=WiFiMode::Disabled; ClientRuntimeState Client{}; AccessPointRuntimeState AccessPoint{}; ScanState Scan=ScanState::Idle; uint64_t Revision=0; };
 
 } // namespace ESPressio::WiFi
+
+ESPRESSIO_ENUM_MAPPING(
+    ESPressio::WiFi::WiFiMode,
+    ESPRESSIO_ENUM_VALUE(ESPressio::WiFi::WiFiMode::Disabled, "disabled"),
+    ESPRESSIO_ENUM_VALUE(ESPressio::WiFi::WiFiMode::Client, "client"),
+    ESPRESSIO_ENUM_VALUE(ESPressio::WiFi::WiFiMode::AccessPoint, "access-point"),
+    ESPRESSIO_ENUM_VALUE(ESPressio::WiFi::WiFiMode::AccessPointClient, "access-point-client")
+)
+
+ESPRESSIO_ENUM_MAPPING(
+    ESPressio::WiFi::AddressMode,
+    ESPRESSIO_ENUM_VALUE(ESPressio::WiFi::AddressMode::DHCP, "dhcp"),
+    ESPRESSIO_ENUM_VALUE(ESPressio::WiFi::AddressMode::Static, "static")
+)
