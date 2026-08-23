4b1bb48ad4aed8c83c474e1de8b882a174c09a18
Simon J Stuart <lakraven@gmail.com>
2026-08-23 18:57:19 +0200

    feat(#15): add APUntilClient observer callback

--- src/ESPressio_IWiFiObserver.hpp
+++ src/ESPressio_IWiFiObserver.hpp
@@ -11,6 +11,7 @@ public:
     virtual void OnWiFiModeChanged(WiFiMode, WiFiMode) {}
     virtual void OnClientStateChanged(const ClientRuntimeState&, const ClientRuntimeState&) {}
     virtual void OnAccessPointStateChanged(const AccessPointRuntimeState&, const AccessPointRuntimeState&) {}
+    virtual void OnAPUntilClientStateChanged(const APUntilClientRuntimeState&, const APUntilClientRuntimeState&) {}
     virtual void OnScanStateChanged(ScanState, ScanState) {}
     virtual void OnScanCompleted(const std::vector<ScanResult>&) {}
     virtual void OnAccessPointStationConnected(const MacAddress&) {}
